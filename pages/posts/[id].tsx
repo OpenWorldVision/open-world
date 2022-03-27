4882d6d07dd377b58347ade308183c813068a4f5
Phuong Nguyen <phuong.nguyennam@digipaysolution.com>
2022-03-27 11:23:35 +0700

    build cloudfare 2

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,26 @@
+import Layout from '@components/layout'
+import { getAllPostIds, getPostData } from '@lib/posts'
+import Head from 'next/head'
+import Date from '@components/date'
+import utilStyles from '../../styles/utils.module.css'
+
+export default function Post({ postData }) {
+  return null
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData,
+    },
+  }
+}
