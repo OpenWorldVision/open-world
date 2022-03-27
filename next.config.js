c4c8041aef50eeb5d1019a43368c398a7e18222c
Phuong Nguyen <phuong.nguyennam@digipaysolution.com>
2022-03-28 01:00:00 +0700

    add next js config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
-// module.exports = {
-//   // https://github.com/vercel/next.js/issues/21079
-//   // Remove this workaround whenever the issue is fixed
-//   images: {
-//     loader: 'imgix',
-//     path: '',
-//   },
-// }
+module.exports = {
+  images: {
+    // limit of 25 deviceSizes values
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    // limit of 25 imageSizes values
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    // limit of 50 domains values
+    domains: [],
+    // path prefix for Image Optimization API, useful with `loader`
+    path: '/_next/image',
+    // loader can be 'default', 'imgix', 'cloudinary', 'akamai', or 'custom'
+    loader: 'default',
+    // disable static imports for image files
+    disableStaticImages: false,
+    // minimumCacheTTL is in seconds, must be integer 0 or more
+    minimumCacheTTL: 60,
+    // ordered list of acceptable optimized image formats (mime types)
+    formats: ['image/webp'],
+    // enable dangerous use of SVG images
+    dangerouslyAllowSVG: false,
+    // set the Content-Security-Policy header
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+}
