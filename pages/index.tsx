f4efdc29ff9c68c22b70c07eb57ea9ceaea33ea7
Tam Nguyen Ngoc <34363701+nntam2013@users.noreply.github.com>
2022-03-29 23:19:55 +0700

    fix: Cloudflare CI (#4)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,8 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '@components/layout'
-import { getSortedPostsData } from '@lib/posts'
 import { Button, Container, Heading, VStack } from '@chakra-ui/react'
 import Entry from './entry/index'
 
 export default function Home({ allPostsData }) {
   return <Entry />
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData,
-    },
-  }
 }
