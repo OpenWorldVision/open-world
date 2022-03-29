a051cc4fb8263612bb6955a05f814265ead19f30
Phuong Nguyen <phuong.nguyennam@digipaysolution.com>
2022-03-29 23:47:11 +0700

    rebuild 2

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Layout, { siteTitle } from '@components/layout'
-import { getSortedPostsData } from '@lib/posts'
+// import { getSortedPostsData } from '@lib/posts'
 import { Button, Container, Heading, VStack } from '@chakra-ui/react'
 
 const Home = () => {
