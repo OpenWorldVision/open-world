e9ea2b1f1222fa29ae1c01f60779ae7c21ecdc8c
Hiệp Nguyễn Quang <18520729@gm.uit.edu.vn>
2022-04-11 15:19:51 +0700

    build Layout alchemist page

---
+++ components/alchemist/alchemistBgAnimate.tsx
@@ -0,0 +1,46 @@
+import styled from '@emotion/styled';
+import React from 'react'
+
+export default function AlchemistBgAnimate() {
+  return (
+    <AlchemistBgAnimateStyle>
+      <div>
+        <div className="alchemist-bg-animate">
+          <img
+            src="/images/alchemist/alchemist-bg-animation.png"
+            alt="alchemist-bg-animate"
+          />
+        </div>
+      </div>
+    </AlchemistBgAnimateStyle>
+  )
+}
+
+const AlchemistBgAnimateStyle = styled.div({
+  '.alchemist-bg-animate': {
+    position: 'absolute',
+    left: '0',
+    top: '0',
+    width: '1312px',
+    height: '992.97px',
+    zIndex: '400',
+    overflow: 'hidden',
+    'img': {
+      position: 'absolute',
+      minWidth: '10560px',
+      height: '973.97px',
+      imageRendering: 'pixelated',
+      transform: 'scale(1, 1)',
+      animation: '0.8s steps(8) 0s infinite normal none running animate-alchemist-bg-animate',
+    },
+    '@keyframes animate-alchemist-bg-animate': {
+      '0%': {
+        left: 0,
+        top: 0,
+      },
+      '100%': {
+        left: '-10560px',
+      }
+    }
+  },
+})
--- components/jeweler/base/bankShineChestBL.tsx
+++ components/jeweler/base/bankShineChestBL.tsx
@@ -1,47 +1,47 @@
 import styled from '@emotion/styled';
 import React from 'react'
 
-export default function BankShineChestBL () {
-    return (
-        <BankShineChestBLStyle>
-            <div>
-                <div className="bankShine-chestBL">
-                    <img
-                        src="/images/jeweler/jeweler/bankShine-chestBL.png"
-                        alt="bankShine-chestBL"
-                    />
-                </div>
-            </div>
-        </BankShineChestBLStyle>
-    )
+export default function BankShineChestBL() {
+  return (
+    <BankShineChestBLStyle>
+      <div>
+        <div className="bankShine-chestBL">
+          <img
+            src="/images/jeweler/bankShine-chestBL.png"
+            alt="bankShine-chestBL"
+          />
+        </div>
+      </div>
+    </BankShineChestBLStyle>
+  )
 }
 
 const BankShineChestBLStyle = styled.div({
-    '.bankShine-chestBL': {
-        position: 'absolute',
-        left: '70px',
-        top: '717.081px',
-        width: '92.8649px',
-        height: '139.297px',
-        zIndex: '400',
-        overflow: 'hidden',
-        'img': {
-            position: 'absolute',
-            minWidth: '8079.24px',
-            height: '139.297px',
-            imageRendering: 'pixelated',
-            transform: 'scale(1, 1)',
-            animation: '8.7s steps(87, end) 0s infinite normal none running animate-bankShine-chestBL',
-        },
-        '@keyframes animate-bankShine-chestBL': {
-            '0%': {
-              left: 0,
-              top: 0,
-            },
-            '100%': {
-              left: '-8079.24px',
-              top: 0,
-            }
-        }
+  '.bankShine-chestBL': {
+    position: 'absolute',
+    left: '70px',
+    top: '717.081px',
+    width: '92.8649px',
+    height: '139.297px',
+    zIndex: '400',
+    overflow: 'hidden',
+    'img': {
+      position: 'absolute',
+      minWidth: '8079.24px',
+      height: '139.297px',
+      imageRendering: 'pixelated',
+      transform: 'scale(1, 1)',
+      animation: '8.7s steps(87, end) 0s infinite normal none running animate-bankShine-chestBL',
     },
+    '@keyframes animate-bankShine-chestBL': {
+      '0%': {
+        left: 0,
+        top: 0,
+      },
+      '100%': {
+        left: '-8079.24px',
+        top: 0,
+      }
+    }
+  },
 })
