aee917db7b3ee928dfecfde4eabc054f497581cf
Hiệp Nguyễn Quang <18520729@gm.uit.edu.vn>
2022-04-19 12:57:50 +0700

    fix positon button world map

--- components/worldmap/BtnWorldMap.tsx
+++ components/worldmap/BtnWorldMap.tsx
@@ -38,10 +38,14 @@ const BtnWorldMapStyle = styled.div({
     '.bordered-box-thin': {
       padding: '0.25rem',
 <<<<<<< HEAD
+<<<<<<< HEAD
       left: '10px',
 =======
       bottom: '100px',
 >>>>>>> 24e7157 (button world map on mobile)
+=======
+      left: '10px',
+>>>>>>> d0ae18a (fix positon button world map)
     },
   },
 })
