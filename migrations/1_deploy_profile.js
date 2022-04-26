4980ef527fc6f6b9a964e4cf49bf21b2144d6048
Tam Nguyen <nntam2013@gmail.com>
2022-04-26 13:55:38 +0700

    feat: add migration

--- migrations/1_deploy_profile.js
+++ migrations/1_deploy_profile.js
@@ -2,10 +2,12 @@ const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades')
 const Profiles = artifacts.require('Profiles')
 
 module.exports = async function (deployer, network) {
-  if (network === 'bsctestnet') {
+  let govToken
+  if (network === 'harmonyTestnet') {
+    profileProxy = '0xe2c25240eA6f598e804BEdcd0a9D7577bB5Fb383'
   }
-
-  if (network === 'bscmainnet') {
+  if (network === 'harmony') {
+    profileProxy = ''
   }
   await deployProxy(Profiles, [], {
     deployer,
---
+++ migrations/3_deploy_openworld.js
@@ -0,0 +1,14 @@
+const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades')
+const Profiles = artifacts.require('Profiles')
+const OpenWorld = artifacts.require('OpenWorld')
+
+module.exports = async function (deployer, network) {
+  let taxAddress
+  if (network === 'harmonyTestnet') {
+    taxAddress = '0x2CC6D07871A1c0655d6A7c9b0Ad24bED8f940517'
+  }
+  if (network === 'harmony') {
+    taxAddress = ''
+  }
+  await deployer.deploy(OpenWorld, 'OpenWorld', 'OPEN', taxAddress)
+}
