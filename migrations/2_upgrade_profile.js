1dda97b7e6d855023ea487bd43ab505b348852c7
Tam Nguyen <nntam2013@gmail.com>
2022-04-30 13:56:24 +0700

    feat:L update migration

--- migrations/2_upgrade_profile.js
+++ migrations/2_upgrade_profile.js
@@ -4,11 +4,13 @@ const Profiles = artifacts.require('Profiles')
 module.exports = async function (deployer, network) {
   let profileProxy
   if (network === 'harmonyTestnet') {
-    profileProxy = '0xEfC8E6EDfeD04fFE7B32a3962BB821f7073e03b3'
+    profileProxy = '0x87461dE8692EAD1de9eE628FF25D97Ae393Ea162'
   }
   if (network === 'harmony') {
     profileProxy = '0x707Ea5fC3Fc92c3B802Ecb9E1428E6F4FF03282f'
   }
+  // const profile = await Profiles.at(profileProxy)
+  // profile.setRequireBalanceProfession(1000)
   await upgradeProxy(profileProxy, Profiles, {
     deployer,
   })
