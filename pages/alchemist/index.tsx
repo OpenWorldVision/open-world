79433c9ea7d8a23aa6981046ad718753884a6b94
Hiệp Nguyễn Quang <18520729@gm.uit.edu.vn>
2022-04-30 08:41:36 +0700

    remove icon mouse

--- pages/alchemist/index.tsx
+++ pages/alchemist/index.tsx
@@ -99,7 +99,7 @@ const AlchemistPage = styled.div({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    cursor: 'url(/images/jeweler/default-cursor.png), auto',
+    // cursor: 'url(/images/jeweler/default-cursor.png), auto',
     overflow: 'hidden',
     '.alchemist': {
       position: 'absolute',
