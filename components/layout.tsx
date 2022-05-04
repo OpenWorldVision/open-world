d550ad3c6a627c0817ed1e7977cc721db813f0a1
Hiệp Nguyễn Quang <18520729@gm.uit.edu.vn>
2022-05-04 23:01:53 +0700

    fix code

--- components/layout.tsx
+++ components/layout.tsx
@@ -60,12 +60,9 @@ export default function Layout({ children, home }) {
         />
         <meta
           property="og:image"
-          content={`https://images.alphacoders.com/605/605592.png${encodeURI(
+          content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
-          )}`}
-          // content={`https://og-image.vercel.app/${encodeURI(
-          //   siteTitle
-          // )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
