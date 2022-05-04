6a22d749eecc27f24d747eede0a35cad7a738bf4
Hiệp Nguyễn Quang <18520729@gm.uit.edu.vn>
2022-05-05 00:40:02 +0700

    fix code

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,7 @@ import Entry from '@components/entry/Entry'
 import { useCallback, useState, useEffect } from 'react'
 import BtnWorldMap from './worldmap/BtnWorldMap'
 
-export const siteTitle = 'Test #Metaverse'
+export const siteTitle = 'Open World #Metaverse'
 
 export default function Layout({ children, home }) {
   const [connected, setConnected] = useState(false)
@@ -56,9 +56,14 @@ export default function Layout({ children, home }) {
         />
         <meta
           property="og:image"
+          // content={`https://og-image.vercel.app/${encodeURI(
+          //   siteTitle
+          // )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=light&md=0&fontSize=75px&images=/images/favicon-3.png`}
+
           // content={`/images/favicon-3${encodeURI(siteTitle)}.png`}
           // content="/images/worldmap/OW-logo.png"
           // content="/images/favicon-3.png"
