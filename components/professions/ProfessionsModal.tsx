d1fb8f22abda2d731cd9f4e5a68dbfe51a9d4774
Trương Đức Vinh <89434186+VinhTruong2001@users.noreply.github.com>
2022-05-25 13:41:51 +0700

    feat: activate career result modal (#67)

--- components/professions/ProfessionsModal.tsx
+++ components/professions/ProfessionsModal.tsx
@@ -1,6 +1,5 @@
 import { Button, Grid, GridItem } from '@chakra-ui/react'
 import { useDispatch } from 'react-redux'
-import { updateIsLoading } from 'reduxActions/isLoadingAction'
 import mainStyle from './professions.module.css'
 import inheritStyle from './professionsSelection.module.css'
 import style from './professionsModal.module.css'
@@ -39,18 +38,19 @@ const npcText = [
 
 type Props = {
   npc: string
+  getResult: (boolean) => void
+  toggleLoadingModal: (boolean) => void
   closeModal: () => void
 }
 
 function ProfessionsModal(props: Props) {
-  const { npc, closeModal } = props
+  const { npc, getResult, toggleLoadingModal, closeModal } = props
   const [currentNpcText, setCurrentNpcText] = useState([])
   const [haveNFT, setHaveNFT] = useState(false)
   const [canActivate, setCanActivate] = useState(false)
   const [currentOPEN, setCurrentOPEN] = useState(0)
   const [requireBalance, setRequireBalance] = useState(0)
   const dispatch = useDispatch()
-
 
   const getContractProfile = async () => {
     const _profile = await getProfile()
@@ -72,14 +72,21 @@ function ProfessionsModal(props: Props) {
 
   const onActivateProfession = async () => {
     if (canActivate) {
-      dispatch(updateIsLoading({ isLoading: true }))
+      toggleLoadingModal(true)
       const professionNft = npcs.indexOf(npc) + 1
       const check = await activateProfession(professionNft)
-      await getContractProfile();
-      dispatch(updateIsLoading({ isLoading: false }))
-      return check
+      if (check) {
+        await getContractProfile()
+        getResult(check)
+        toggleLoadingModal(false)
+      } else {
+        setTimeout(() => {
+          toggleLoadingModal(false)
+        }, 5000)
+      }
+    } else {
+      getResult(false)
     }
-    return null
   }
 
   const checkIfHasNTF = async () => {
@@ -106,8 +113,7 @@ function ProfessionsModal(props: Props) {
   const initialize = async () => {
     await getRequireBalanceProfession()
     await checkIfCanActive()
-
-    dispatch(updateIsLoading({ isLoading: false }))
+    toggleLoadingModal(false)
   }
 
   useEffect(() => {
