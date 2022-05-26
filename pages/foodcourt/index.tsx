8391270369ab8ff31b0d75d716803ba614746599
Nguyễn Quang Hiệp <81562398+QuangHiep2000@users.noreply.github.com>
2022-05-26 13:45:40 +0700

    feat: food court UI (#68)
    * setup food court page
    
    * food court page

---
+++ pages/foodcourt/index.tsx
@@ -0,0 +1,218 @@
+import { Button, Link, styled, Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr } from '@chakra-ui/react'
+import styles from '@components/foodcourt/foodcourt.module.css'
+
+import { useCallback, useEffect, useState } from 'react'
+import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
+import BuyerBoard from '@components/foodcourt/BuyerBoard'
+
+const listSushi = [
+  {
+    'seller': 'alex',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'alex',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'alex',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'alex',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'alex',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'jennei',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'alex',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'jennei',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'jennei',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'jennei',
+    'price': 10,
+    'available': 500
+  },
+  {
+    'seller': 'rose',
+    'price': 10,
+    'available': 1000
+  }
+]
+
+const listFish = [
+  {
+    'seller': 'peter',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'peter',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'peter',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'peter',
+    'price': 10,
+    'available': 1000
+  },
+  {
+    'seller': 'peter',
+    'price': 10,
+    'available': 1000
+  }
+]
+
+export default function FoodCourt() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [isItemBoard, setIsItemBoard] = useState('sushi')
+  const [listItemsBoard, setListItemsBoard] = useState(listSushi)
+  const [isOpenBuyBoard, setIsOpenBuyBoard] = useState(false)
+  const [pageFoodCourt, setPageFoodCourt] = useState(1)
+  const [buyDetail, setBuyDetail] = useState({})
+
+  const toggleBuyModal = useCallback((state) => {
+    setIsOpenBuyBoard(state)
+  }, [])
+
+  const handleSelectItemBoard = useCallback((item) => () => {
+    setIsItemBoard(item)
+    setListItemsBoard(item === 'sushi' ? listSushi : listFish)
+  }, [])
+
+  const handleIncreasePage = useCallback(() => {
+    if (listItemsBoard.slice(pageFoodCourt * 5).length !== 0) {
+      setPageFoodCourt(pageFoodCourt + 1)
+    }
+  }, [pageFoodCourt])
+
+  const handlePreviousPage = useCallback(() => {
+    if (pageFoodCourt > 1) {
+      setPageFoodCourt(pageFoodCourt - 1)
+    }
+  }, [pageFoodCourt])
+
+  const handleRefresh = useCallback(() => {
+    setPageFoodCourt(1)
+  }, [])
+
+  const handleBuyItem = useCallback((available, price) => () => {
+    setBuyDetail({
+      'available': available,
+      'price': price,
+      'itemName': isItemBoard
+    })
+    toggleBuyModal(true)
+  }, [])
+
+  useEffect(() => {
+    const checkWindowWidth = () => {
+      setWindowWidth(window.innerWidth)
+    }
+
+    checkWindowWidth()
+
+    window.addEventListener('resize', checkWindowWidth)
+
+    return () => {
+      window.removeEventListener('resize', checkWindowWidth)
+    }
+  }, [])
+
+  return (
+    <div className={styles.foodCourtContainer}>
+      <div className={styles.foodCourtBg}>
+        <div className={styles.foodCourtTitleContainer}>
+          <div className={styles.navFoodCourt}></div>
+          <div className={styles.foodCourtTitle}></div>
+          <div className={styles.navFoodCourt}></div>
+        </div>
+        <div>
+          <Button className={`${styles.buyBtn} click-cursor`}></Button>
+          <Button onClick={handleSelectItemBoard('sushi')} className={`click-cursor ${styles.foodBtn}`}>
+            <div className={styles.sushiBtn}></div>
+            <div className={`${isItemBoard === 'sushi' && styles.itemBoardSelected}`}></div>
+          </Button>
+          <Button onClick={handleSelectItemBoard('fish')} className={`click-cursor ${styles.foodBtn}`}>
+            <div className={styles.fishBtn}></div>
+            <div className={`${isItemBoard === 'fish' && styles.itemBoardSelected}`}></div>
+          </Button>
+        </div>
+        <div className={styles.foodCourtBoard}>
+          <TableContainer className={styles.table}>
+            <Table size='sm' variant='strunstylediped'>
+              <TableCaption className={styles.paginationContainer}>
+                <div className={styles.pagination}>
+                  <div onClick={handlePreviousPage} className={`${styles.increase} click-cursor`}></div>
+                  <div className={styles.numberPage}>{pageFoodCourt < 10 && 0}{pageFoodCourt}</div>
+                  <div onClick={handleIncreasePage} className={`${styles.previous} click-cursor`}></div>
+                </div>
+              </TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>
+                    <div className={styles.columnTitle}>SELLER</div>
+                  </Th>
+                  <Th><div className={styles.columnTitle}>PRICE</div></Th>
+                  <Th><div className={styles.columnTitle}>AVAILABLE</div></Th>
+                  <Th sx={{ textAlign: 'center' }}><Button onClick={handleRefresh} className={`${styles.refreshBtn} click-cursor`}></Button></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {listItemsBoard.slice((pageFoodCourt - 1) * 5).map((item, index) => {
+                  if (index < 5) {
+                    return <>
+                      <Tr>
+                        <Td><div className={styles.columnItem}>{item.seller}</div></Td>
+                        <Td><div className={styles.columnItem}>{item.price} OPEN</div></Td>
+                        <Td><div className={styles.columnItem}>{item.available}</div></Td>
+                        <Td sx={{ textAlign: 'center' }}><Button onClick={handleBuyItem(item.available, item.price)} className={`${styles.buyBtnItem} click-cursor`}></Button></Td>
+                      </Tr>
+                    </>
+                  }
+                })}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </div>
+        <BuyerBoard
+          isOpen={isOpenBuyBoard}
+          toggleModalBuyModal={() => toggleBuyModal(false)}
+          buyDetail={buyDetail}
+        />
+        <Link href="/">
+          <a className={`${styles.backBtn} click-cursor`}></a>
+        </Link>
+      </div>
+    </div>
+
+  )
+} 
--- pages/professions/blacksmith/index.tsx
+++ pages/professions/blacksmith/index.tsx
@@ -4,54 +4,14 @@ import { useEffect, useState } from 'react'
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
 
 export default function BlackSmith() {
-  const [isLoading, setIsLoading] = useState(true)
-  // const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-
-  // useEffect(() => {
-  // const checkWindowWidth = () => {
-  //   setWindowWidth(window.innerWidth)
-  // }
-
-  //   checkWindowWidth()
-
-  //   window.addEventListener('resize', checkWindowWidth)
-
-  //   return () => {
-  //     window.removeEventListener('resize', checkWindowWidth)
-  //   }
-  // }, [])
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setIsLoading(false)
-  //   }, 2000)
-  // }, [])
   return (
     <div className={styles.blackSmithContainer}>
-      {/* <div className={`${!isLoading && styles.loadedBlackSmith}`}>
-        <div className={`overlay ${styles.preLoaderBlackSmith}`}>
-          <div className={styles.preloaderFoldingCube}>
-            <div className={`${styles.preloaderCube1} ${styles.preloaderCube}`}></div>
-            <div className={`${styles.preloaderCube2} ${styles.preloaderCube}`}></div>
-            <div className={`${styles.preloaderCube4} ${styles.preloaderCube}`}></div>
-            <div className={`${styles.preloaderCube3} ${styles.preloaderCube}`}></div>
-          </div>
-        </div>
-      </div> */}
       <TransformWrapper
         initialPositionX={0}
         initialPositionY={0}
         centerOnInit={true}
-      // wheel={{
-      //   disabled: true,
-      // }}
-      // doubleClick={{
-      //   disabled: true,
-      // }}
-      // panning={{
-      //   disabled: windowWidth >= 1858,
-      // }}
       >
-        <TransformComponent wrapperStyle={{ height: '100vh', width: '100vw' }}>
+        <TransformComponent>
           <div className={styles.blackSmithBg}>
             <Link href="/professions/blacksmith/sellerboard">
               <a className={`${styles.sellHammer}`}></a>
@@ -59,8 +19,6 @@ export default function BlackSmith() {
             <Link href="/professions/blacksmith/forgehammer">
               <a className={`${styles.forgeHammer}`}></a>
             </Link>
-            {/* <div className={styles.sellHammer}></div> */}
-            {/* <div className={styles.forgeHammer}></div> */}
           </div>
         </TransformComponent>
       </TransformWrapper>
