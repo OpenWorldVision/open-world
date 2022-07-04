7ff90b01594be5c59c6c7f0cddb571a9ae890a31
Phuong Nguyen <phuong.nguyennam@digipaysolution.com>
2022-07-05 00:53:41 +0700

    Update UI castle mobile

--- components/castle/mobile/CastleLayout.tsx
+++ components/castle/mobile/CastleLayout.tsx
@@ -1,41 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
 import styles from './CastleLayout.module.css'
-import {
-  fetchProfessionsNFTAmount,
-  fetchProfessionsNFTPrices,
-} from 'utils/professions'
+import {} from 'utils/professions'
 
-const CastleLayout = ({ onPressBuyNFT }) => {
-  const [nftsAmount, setNftsAmount] = useState({
-    openianAmount: 0,
-    supplierAmount: 0,
-    blacksmithAmount: 0,
-  })
-  const [nftsPrices, setNftsPrices] = useState({
-    openianPrice: '0',
-    supplierPrice: '0',
-    blacksmithPrice: '0',
-  })
-
-  const fetchNFTAmount = useCallback(async () => {
-    const amount = await fetchProfessionsNFTAmount()
-    setNftsAmount(amount)
-  }, [])
-
-  const fetchNFTPrices = useCallback(async () => {
-    const prices = await fetchProfessionsNFTPrices()
-    setNftsPrices(prices)
-  }, [])
-  const initialize = async () => {
-    await fetchNFTAmount()
-    await fetchNFTPrices()
-  }
-
-  useEffect(() => {
-    initialize()
-  }, [])
-
+const CastleLayout = ({ onPressBuyNFT, listNFTs }) => {
   const _onPressBuyNFT = useCallback(
     (nft) => {
       onPressBuyNFT(nft)
@@ -65,29 +32,6 @@ const CastleLayout = ({ onPressBuyNFT }) => {
     [_onPressBuyNFT]
   )
 
-  const listNFTs = [
-    {
-      id: 'openian',
-      name: 'openian',
-      image: '/images/professions/npc/openianNPC.webp',
-      price: nftsPrices.openianPrice,
-      available: nftsAmount.openianAmount,
-    },
-    {
-      id: 'supplier',
-      name: 'supplier',
-      image: '/images/professions/npc/supplierNPC.webp',
-      price: nftsPrices.supplierPrice,
-      available: nftsAmount.supplierAmount,
-    },
-    {
-      id: 'blacksmith',
-      name: 'blacksmith',
-      image: '/images/professions/npc/smithNPC.webp',
-      price: nftsPrices.blacksmithPrice,
-      available: nftsAmount.blacksmithAmount,
-    },
-  ]
   return (
     <div className={styles.container}>
       <div className={styles.backgroundContainer}>
@@ -98,6 +42,9 @@ const CastleLayout = ({ onPressBuyNFT }) => {
       </div>
       <div className={styles.containerList}>
         <div className={styles.containerListInside}>
+          <div className={styles.headerBox}>
+            <p>Selling Items</p>
+          </div>
           {listNFTs.map((nft) => {
             return (
               <div key={nft.id} className={styles.containerClassImage}>
--- pages/castle/index.tsx
+++ pages/castle/index.tsx
@@ -1,14 +1,34 @@
-import { useRef, useState, useCallback, useEffect } from 'react'
+import { useRef, useState, useCallback, useEffect, useMemo } from 'react'
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
 import style from '@components/castle/castle.module.css'
 import CastleModal from '@components/castle/CastleModal'
 import LandAuction from '@components/castle/LandAuction'
-import { ButtonGroup, Button, Flex, useMediaQuery } from '@chakra-ui/react'
+import {
+  ButtonGroup,
+  Button,
+  Flex,
+  useMediaQuery,
+  useDisclosure,
+  useToast,
+} from '@chakra-ui/react'
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '@components/castle/mobile/MobileHeaderBar.module.css'
 import MobileHeaderBar from '@components/castle/mobile/MobileHeaderBar'
 import CastleLayout from '@components/castle/mobile/CastleLayout'
+import ModalBuyItem from '@components/ModalBuyItem/ModalBuyItem'
+import useTransactionState, {
+  TRANSACTION_STATE,
+} from 'hooks/useTransactionState'
+import { getOpenBalance } from 'utils/checkBalanceOpen'
+import {
+  fetchProfessionsNFTAmount,
+  fetchProfessionsNFTPrices,
+  mintProfessionNFT,
+} from 'utils/professions'
+import LoadingModal from '@components/LoadingModal'
+import { useDispatch } from 'react-redux'
+import { setOpenBalance } from 'reduxActions/profileAction'
 
 export default function Castle() {
   // Ref
@@ -21,8 +41,70 @@ export default function Castle() {
   const [action, setAction] = useState(0)
 
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [isLoading, setIsLoading] = useState(false)
   const [isMobile] = useMediaQuery('(max-width: 1014px)')
   const [showModalBuy, setShowModalBuy] = useState(false)
+  const [selectedNFT, setSelectedNFT] = useState(null)
+  const handleTxStateChange = useTransactionState()
+  const { onClose } = useDisclosure()
+  const toast = useToast()
+
+  const [nftsAmount, setNftsAmount] = useState({
+    openianAmount: 0,
+    supplierAmount: 0,
+    blacksmithAmount: 0,
+  })
+  const [nftsPrices, setNftsPrices] = useState({
+    openianPrice: '0',
+    supplierPrice: '0',
+    blacksmithPrice: '0',
+  })
+
+  const fetchNFTAmount = useCallback(async () => {
+    const amount = await fetchProfessionsNFTAmount()
+    setNftsAmount(amount)
+  }, [])
+
+  const fetchNFTPrices = useCallback(async () => {
+    const prices = await fetchProfessionsNFTPrices()
+    setNftsPrices(prices)
+  }, [])
+
+  const dispatch = useDispatch()
+
+  const getListNFTs = useMemo(() => {
+    const listNFTs = [
+      {
+        id: 'openian',
+        name: 'Openian NFT',
+        image: '/images/professions/npc/openianNPC.webp',
+        price: nftsPrices.openianPrice,
+        available: nftsAmount.openianAmount,
+      },
+      {
+        id: 'supplier',
+        name: 'Supplier NFT',
+        image: '/images/professions/npc/supplierNPC.webp',
+        price: nftsPrices.supplierPrice,
+        available: nftsAmount.supplierAmount,
+      },
+      {
+        id: 'blacksmith',
+        name: 'Blacksmith NFT',
+        image: '/images/professions/npc/smithNPC.webp',
+        price: nftsPrices.blacksmithPrice,
+        available: nftsAmount.blacksmithAmount,
+      },
+    ]
+    return listNFTs
+  }, [
+    nftsAmount.blacksmithAmount,
+    nftsAmount.openianAmount,
+    nftsAmount.supplierAmount,
+    nftsPrices.blacksmithPrice,
+    nftsPrices.openianPrice,
+    nftsPrices.supplierPrice,
+  ])
 
   useEffect(() => {
     const checkWindowWidth = () => {
@@ -30,6 +112,10 @@ export default function Castle() {
     }
 
     checkWindowWidth()
+    if (isMobile) {
+      fetchNFTAmount()
+      fetchNFTPrices()
+    }
 
     window.addEventListener('resize', checkWindowWidth)
 
@@ -42,27 +128,101 @@ export default function Castle() {
     setAction(action)
     setIsLandAuctionOpen(true)
   }, [])
+  const closeModal = useCallback(() => {
+    setShowModalBuy(false)
+  }, [setShowModalBuy])
 
-  const _onBuyNFT = useCallback((nft) => {
-    //get nft to open modal
-  }, [])
+  const _onBuyNFT = useCallback(
+    (nft) => {
+      //get nft to open modal
+      setShowModalBuy(true)
+      setSelectedNFT(nft)
+    },
+    [setShowModalBuy, setSelectedNFT]
+  )
 
   const renderMobileUI = useCallback(() => {
     return (
       <>
         <MobileHeaderBar />
-        <CastleLayout onPressBuyNFT={_onBuyNFT} />
+        <CastleLayout onPressBuyNFT={_onBuyNFT} listNFTs={getListNFTs} />
       </>
     )
-  }, [])
+  }, [_onBuyNFT, getListNFTs])
+  const _mintProfessionsNFT = useCallback(
+    async (trait) => {
+      setShowModalBuy(false)
+      setIsLoading(true)
+      const balance = parseFloat(await getOpenBalance(false))
+      const NTFCardPrice = parseFloat(selectedNFT?.price)
+      if (balance >= NTFCardPrice) {
+        const title = 'Purchase NFT card'
+        const data = await mintProfessionNFT(trait, (txHash) => {
+          handleTxStateChange(title, txHash, TRANSACTION_STATE.WAITING)
+        })
+        if (data) {
+          handleTxStateChange(title, data.transactionHash, data.status)
+          // await fetchNFTAmount()
+        } else {
+          handleTxStateChange(title, '', TRANSACTION_STATE.NOT_EXECUTED)
+        }
+      } else {
+        toast({
+          title: 'Purchase NFT card transaction is failed to excute',
+          description: "You don't have enough OPEN to purchase",
+          duration: 10000,
+          isClosable: true,
+          status: 'error',
+        })
+        onClose()
+      }
+      fetchNFTAmount()
+      fetchNFTPrices()
+      setIsLoading(false)
+      const newBalance = await getOpenBalance(true)
+      dispatch(setOpenBalance(newBalance))
+    },
+    [
+      dispatch,
+      fetchNFTAmount,
+      fetchNFTPrices,
+      handleTxStateChange,
+      onClose,
+      selectedNFT?.price,
+      toast,
+    ]
+  )
+
+  const _confirmBuy = useCallback(() => {
+    const id =
+      selectedNFT?.id === 'openian' ? 1 : selectedNFT?.id === 'supplier' ? 2 : 3
+    _mintProfessionsNFT(id)
+  }, [_mintProfessionsNFT, selectedNFT])
 
   return (
-    <div className={`${style.castleOverlay} overlay`}>
+    <div
+      className={
+        !isMobile
+          ? `${style.castleOverlay} overlay`
+          : `${style.castleMobileOverlay} overlay`
+      }
+    >
+      {isLoading && <LoadingModal />}
       <Head>
-        <title>{!isMobile ? 'Castle' : 'Mobile Castle'}</title>
+        <title>Castle</title>
       </Head>
       {isMobile ? (
-        renderMobileUI()
+        <>
+          {renderMobileUI()}
+          <ModalBuyItem
+            isOpen={showModalBuy}
+            nft={selectedNFT}
+            onClose={closeModal}
+            fromCastle={true}
+            title={'Buy Items'}
+            confirmBuy={_confirmBuy}
+          />
+        </>
       ) : (
         <div className={styles.webContainer}>
           <div
@@ -129,4 +289,7 @@ export default function Castle() {
       />
     </div>
   )
+}
+function trait(trait: any, arg1: (txHash: any) => void) {
+  throw new Error('Function not implemented.')
 }
