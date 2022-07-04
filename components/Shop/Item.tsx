2173d9a330f1345cf206298fbb8f22a7949b4141
Tam Nguyen Ngoc <34363701+nntam2013@users.noreply.github.com>
2022-07-04 14:40:13 +0700

    feat: add shop ui mobile (#145)
    * feat: add shop ui mobile
    
    * feat: change button size
    
    * feat: update UI & logic
    
    * feat: mt item

--- components/Shop/Item.tsx
+++ components/Shop/Item.tsx
@@ -1,8 +1,9 @@
-import { Text, Box, Image } from '@chakra-ui/react'
+import { Text, Box, Image, useMediaQuery } from '@chakra-ui/react'
 import Button from '@components/theme/components/Button'
 import { utils } from 'ethers'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { getHammerPrice, isBoughtHammer } from 'utils/Item'
+import Price from './Price'
 
 type Props = {
   onBuy: () => void
@@ -10,6 +11,12 @@ type Props = {
 function Item({ onBuy }: Props) {
   const [price, setPrice] = useState('0')
   const [isBought, setIsBought] = useState(true)
+  const [isMobile] = useMediaQuery('(max-width: 1014px)')
+
+  const handleBuy = useCallback(() => {
+    setIsBought(true)
+    onBuy()
+  }, [onBuy])
 
   useEffect(() => {
     ;(async () => {
@@ -23,55 +30,78 @@ function Item({ onBuy }: Props) {
   return (
     <Box
       display="flex"
-      alignItems="center"
+      alignItems="flex-end"
       justifyContent="space-between"
-      bgColor="#ECECEC"
-      p="6px 12px"
+      bgColor="#DCD7C1"
+      p="16px 12px"
       borderRadius={10}
       mt={4}
       mb={4}
     >
-      <Box bgColor="white" p={0.4} borderWidth={2} minW={68} mr={1}>
-        <Image
-          src="/images/shop/icon-sushi-1.webp"
-          width={68}
-          height={68}
-          alt="icon-sushi"
-          p={2}
-          background="linear-gradient(180deg, rgba(129, 129, 129, 0.7) 0%, rgba(213, 213, 213, 0.7) 100%)"
-        />
+      <Box display="flex" gap={1}>
+        <Box bgColor="white" p={0.4} borderWidth={2} minW={68} mr={1}>
+          <Image
+            src="/images/shop/stone-pickaxe.webp"
+            width={68}
+            height={68}
+            alt="stone-pickaxe"
+            p={2}
+            background="linear-gradient(180deg, rgba(90, 90, 90, 0.7) 0%, rgba(171, 171, 171, 0.7) 100%);"
+          />
+        </Box>
+
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+        >
+          <Text fontSize="14" fontWeight="medium">
+            Stone Pickaxe
+          </Text>
+          {!isMobile && (
+            <Text fontSize={12}>
+              necessary tools for mining, made of black rock in the steep
+              mountains.
+            </Text>
+          )}
+          {isMobile && <Price price={Number(price)} />}
+        </Box>
       </Box>
 
-      <Box maxW="60">
-        <Text fontSize="md" fontWeight="medium">
-          Stone Pickaxe
-        </Text>
-        <Text fontSize={12}>
-          necessary tools for mining, made of black rock in the steep mountains.
-        </Text>
-      </Box>
-
+      {!isMobile && (
+        <Box
+          bgColor="#D9D9D9"
+          p="8px 16px"
+          borderRadius={10}
+          mt={2}
+          minW={110}
+          mr={1}
+        >
+          <Text fontWeight="medium" fontSize={14}>
+            Price {price}
+          </Text>
+        </Box>
+      )}
       <Box
-        bgColor="#D9D9D9"
-        p="8px 16px"
-        borderRadius={10}
-        mt={2}
-        minW={110}
-        mr={1}
+        display="flex"
+        flexDirection="column"
+        fontWeight="medium"
+        fontSize="10"
+        color="#6A6A6A"
+        alignItems="flex-end"
       >
-        <Text fontWeight="medium" fontSize={14}>
-          Price {price}
-        </Text>
+        <Text>Available: {isBought ? 0 : 1}</Text>
+        <Button
+          bgColor="#472805"
+          color="#F0E0D0"
+          onClick={handleBuy}
+          disabled={isBought}
+          mt="8px"
+          size="sm"
+        >
+          {isBought ? 'Sold Out' : 'Buy'}
+        </Button>
       </Box>
-
-      <Button
-        bgColor="#472805"
-        color="white"
-        onClick={onBuy}
-        disabled={isBought}
-      >
-        {isBought ? 'Sold Out' : 'Buy'}
-      </Button>
     </Box>
   )
 }
--- components/Shop/index.tsx
+++ components/Shop/index.tsx
@@ -8,6 +8,8 @@ import {
   Box,
   Image,
   forwardRef,
+  useBreakpointValue,
+  useMediaQuery,
 } from '@chakra-ui/react'
 import { useImperativeHandle, useRef } from 'react'
 import ConfirmationModal, { ConfirmationModalRef } from './ConfirmationModal'
@@ -21,6 +23,12 @@ export type ShopRef = {
 function Shop(_, ref) {
   const { isOpen, onToggle } = useDisclosure()
   const confirmationRef = useRef<ConfirmationModalRef>()
+  const flexDirection: 'row' | 'column' = useBreakpointValue({
+    xl: 'row',
+    lg: 'row',
+    base: 'column',
+  })
+  const [isMobile] = useMediaQuery('(max-width: 1014px)')
 
   useImperativeHandle(
     ref,
@@ -33,37 +41,85 @@ function Shop(_, ref) {
 
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onToggle} size="6xl" isCentered>
-        <ModalOverlay />
-        {/* <ModalCloseButton /> */}
-        <ModalContent bgColor="transparent">
+      <Modal isOpen={isOpen} onClose={onToggle} size="6xl">
+        <ModalOverlay bg="blackAlpha.800" />
+
+        <ModalContent bgColor="transparent" mt="5rem">
           <ModalBody>
-            <Box display="flex" justifyContent="space-between" gap={8}>
-              <Box bgColor="#D9D9D9" borderRadius={10} p={0} flex={2}>
-                <Image
-                  src="/images/shop/left-side.webp"
-                  alt="left-side-shop"
-                  w="100%"
-                />
-                <Text textAlign="center" fontSize={14} m={8} pb={8}>
-                  The shop was set up by the government, providing essential
-                  items to support new residents.
-                </Text>
-              </Box>
-              <Box bgColor="#D9D9D9" borderRadius={10} p={4} flex={3}>
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              gap={8}
+              flexDirection={flexDirection}
+            >
+              {isMobile ? (
+                <Box bgColor="#D9D9D9" borderRadius={10} p={0} flex={2}>
+                  <Image
+                    src="/images/shop/main-side.webp"
+                    alt="left-side-shop"
+                    w="100%"
+                  />
+                  <Text
+                    textAlign="center"
+                    fontSize={14}
+                    m={8}
+                    pb={8}
+                    position="absolute"
+                    top={0}
+                    color="white"
+                    fontWeight="bold"
+                  >
+                    The shop was set up by the government, providing essential
+                    items to support new residents.
+                  </Text>
+                </Box>
+              ) : (
+                <Box bgColor="#D9D9D9" borderRadius={10} p={0} flex={2}>
+                  <Image
+                    src="/images/shop/left-side.webp"
+                    alt="left-side-shop"
+                    w="100%"
+                  />
+                  <Text textAlign="center" fontSize={14} m={8} pb={8}>
+                    The shop was set up by the government, providing essential
+                    items to support new residents.
+                  </Text>
+                </Box>
+              )}
+              <Box
+                bgColor="#C8BB98"
+                borderRadius={10}
+                p={2}
+                flex={3}
+                position="relative"
+                minH="40vh"
+              >
                 <Box
                   position="absolute"
-                  right={'25%'}
-                  top={-2}
-                  bgColor="#CFB183"
-                  p="8px 12px"
-                  borderRadius={10}
+                  right={0}
+                  left={0}
+                  ml="auto"
+                  mr="auto"
+                  top={-18}
+                  display="flex"
+                  alignItems="center"
+                  w="fit-content"
                 >
-                  <Text fontSize={16} fontWeight="black">
+                  <Text
+                    fontSize={16}
+                    fontWeight="bold"
+                    textAlign="center"
+                    bgColor="#CFB183"
+                    w="-moz-fit-content"
+                    p="8px 12px"
+                    borderRadius={10}
+                  >
                     Selling Items
                   </Text>
                 </Box>
-                <Item onBuy={confirmationRef.current?.open} />
+                <Box mt={8}>
+                  <Item onBuy={confirmationRef.current?.open} />
+                </Box>
               </Box>
             </Box>
           </ModalBody>
